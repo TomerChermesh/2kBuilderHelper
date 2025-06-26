ac003303071e071297548afdc1952cb9120a4a2d
TomerChermesh <tomerchermesh@gmail.com>
2025-06-26 10:13:18 +0300

    Python

--- src/utils/instructions.py
+++ src/utils/instructions.py
@@ -1,5 +1,6 @@
 import os
 
+from src.constants.files import JSON_EXTENSION
 from src.utils.files import load_json_file
 
 INSTRUCTIONS_DIR = os.path.join('../../resources', 'instructions')
@@ -9,8 +10,9 @@ def get_instructions_versions_list() -> list[str]:
     files_names: list[str] = os.listdir(INSTRUCTIONS_DIR)
     instructions_versions_list: list[str] = []
     for file_name in files_names:
-        if file_name.endswith('.json'):
-            instructions_versions_list.append(file_name[:-5])
+        if file_name.endswith(JSON_EXTENSION):
+            version_name: str = file_name[:-len(JSON_EXTENSION)]
+            instructions_versions_list.append(version_name)
 
     return instructions_versions_list
 
